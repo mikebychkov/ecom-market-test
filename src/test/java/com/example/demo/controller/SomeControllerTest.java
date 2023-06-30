b302bc8fe5fc47c1cd91fc02ec5e23a1d651fde5
Mike <mihailbichkov@yandex.ru>
2023-06-30 21:01:41 +0500

    test description

--- src/test/java/com/example/demo/controller/SomeControllerTest.java
+++ src/test/java/com/example/demo/controller/SomeControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -19,7 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class SomeControllerTest {
 
     @Autowired
